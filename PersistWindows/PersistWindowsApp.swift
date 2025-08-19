5be6de8463ed6f36d3717a3e7548c5dc5a21b4a3
Rotem Cohen <rotembcohen@gmail.com>
2025-08-19 17:25:26 -0400

    Initial Commit

---
+++ PersistWindows/PersistWindowsApp.swift
@@ -0,0 +1,33 @@
+//
+//  PersistWindowsApp.swift
+//  PersistWindows
+//
+//  Created by Rotem Cohen on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct PersistWindowsApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
